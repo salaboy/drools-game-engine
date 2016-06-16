0d5546fef5b86298dae746a23171c5918e140dfd
salaboy <salaboy@gmail.com>
2016-06-16 10:21:47 +0200

    - adding NO CDI test

---
+++ drools-game-engine-rules/src/test/java/org/drools/workshop/rules/tests/E_NoCDIRulesTest.java
@@ -0,0 +1,46 @@
+package org.drools.workshop.rules.tests;
+
+import java.util.ArrayList;
+import org.drools.workshop.model.house.House;
+import org.hamcrest.Matchers;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ *
+ * @author salaboy
+ * 
+ * This test shows how to bootstrap the Rule Engine in a no CDI environment.
+ * Just plain JUnit is used to run this. 
+ */
+public class E_NoCDIRulesTest {
+
+    @Test
+    public void helloDroolsTest() {
+
+        System.out.println( "Bootstrapping the Rule Engine ..." );
+        // Bootstrapping a Rule Engine Session
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieBase kBase = kContainer.getKieBase( "confKBase" );
+
+        KieSession kSession = kBase.newKieSession();
+        ArrayList<String> errors = new ArrayList<String>();
+        kSession.setGlobal( "errors", errors );
+        House house = new House( "Manic Mansion" );
+        kSession.insert( house );
+
+        int fired = kSession.fireAllRules();
+        assertEquals( 1, fired );
+        assertEquals( 1, errors.size() );
+        assertThat( errors, Matchers.contains( "Warn: Your House ( " + house.getName() + " ) has no Rooms" ) );
+
+        kSession.dispose();
+
+    }
+}
