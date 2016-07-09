72c0fd4e90f02501becf527e4aef0e0839e6fa93
Antonio Mendoza Pérez <antmendoza@gmail.com>
2016-07-10 00:22:09 +0100

    Adding rest service test

--- drools-game-engine-services/src/test/java/com/drools/game/services/endpoint/test/GameServiceTest.java
+++ drools-game-engine-services/src/test/java/com/drools/game/services/endpoint/test/GameServiceTest.java
@@ -1,36 +1,35 @@
 package com.drools.game.services.endpoint.test;
 
-import java.util.List;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.drools.game.core.api.GameConfiguration;
+import org.drools.game.model.api.Player;
+import org.drools.game.services.endpoint.api.GameService;
+import org.drools.game.services.endpoint.impl.GameServiceImpl;
 import org.drools.game.services.infos.GameSessionInfo;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.wildfly.swarm.jaxrs.JAXRSArchive;
 
 @RunWith(Arquillian.class)
-@Ignore
-public class GameServiceTest 
-{
+public class GameServiceTest {
 
 	private Client client;
-	
+
 	@Before
 	public void setup() throws Exception {
 		client = ClientBuilder.newClient();
-
 	}
 
 	@After
@@ -38,25 +37,21 @@ public class GameServiceTest
 		client.close();
 	}
 
-    @Deployment
-    public static JavaArchive createDeployment() {
 
-        JavaArchive jar = ShrinkWrap.create( JavaArchive.class )
-                .addAsManifestResource( EmptyAsset.INSTANCE, "beans.xml" );
-//        System.out.println( jar.toString( true ) );
-        return jar;
-    }
-
-    
+	@Deployment
+	public static Archive createDeployment() {
+		JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
+		deployment.setContextRoot("/api");
+		deployment.addClass(GameService.class).addClass(GameServiceImpl.class).addClass(GameSessionInfo.class)
+				.addPackage(GameConfiguration.class.getPackage()).addPackage(Player.class.getPackage());
+		return deployment;
+	}
 
 	@Test
-	public void getAllGameSessions() {
+	public void checkService() {
 		WebTarget target = client.target("http://localhost:8080/api/game");
-
-		List<GameSessionInfo> sessions = target.request(MediaType.APPLICATION_JSON)
-				.get(new GenericType<List<GameSessionInfo>>() {
-				});
-		org.junit.Assert.assertEquals(0, sessions.size());
+		Response response = target.request(MediaType.APPLICATION_JSON).get();
+		Assert.assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 	}
 
 }
